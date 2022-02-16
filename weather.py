aa50ca84de441e3782662a4721531cba8500cda5
mingyeong <mingang0830@gmail.com>
2022-02-16 15:50:41 +0900

    access token 분리

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import json
 
 
 url = "https://www.weather.go.kr/w/weather/forecast/short-term.do?stnId=109"
-
+access_token = "o0sFFZ7dlOI5Jv-4wTPbDpghUGc8wWnE0SaZuwo9dNoAAAF_AO36lQ"
 
 def get_data():
     r = requests.get(url)
@@ -15,7 +15,7 @@ def get_data():
     return summary_data
 
 
-def send_me_via_kakaotalk(data):
+def send_me_via_kakaotalk(data, access_token):
     template_object = {
         "object_type": 'text',
         "text": data,
@@ -23,14 +23,14 @@ def send_me_via_kakaotalk(data):
             "web_url": url,
             "mobile_web_url": url,
         },
-        "button_title": "기상청 홈페이지"
+        "button_title": "더보기"
     }
-    access_token = "o0sFFZ7dlOI5Jv-4wTPbDpghUGc8wWnE0SaZuwo9dNoAAAF_AO36lQ"
     response = requests.post("https://kapi.kakao.com/v2/api/talk/memo/default/send",
                              headers={"Authorization": f"Bearer {access_token}"},
                              data={"template_object": json.dumps(template_object)})
     
 def parse(data):
+  data = data[1:-1]
   html_tag = re.compile('<.*?>')
   result = re.sub(html_tag, '', data)
   return result
@@ -39,6 +39,4 @@ def parse(data):
 
 if __name__ == "__main__":
     data = parse(str(get_data()))
-    send_me_via_kakaotalk(data)
-    print(data)
-    
+    send_me_via_kakaotalk(data, access_token)
